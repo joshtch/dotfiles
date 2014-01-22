b20cf7ccd4dc43bcce2b26d3c657c5ea61d5e4cb
joshtch <howejosh@gmail.com>
2014-01-22 02:54:49 -0500

    Git aliases + other goodies

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -14,11 +14,47 @@ alias sudo='sudo ' # make sudo play nice with other aliases
 alias _='sudo'
 alias please='sudo'
 
+# git shortcuts {{{
 alias grep='grep --color=auto'
-alias g='grep -in'
+
+alias g='git'
+alias ga='git add'
+alias gr='git rm'
+
+alias gf='git fetch'
+alias gu='git pull'
+alias gup='git pull && git push'
+
+alias gs='git status --short'
+alias gd='git diff'
+alias gds='git diff --staged'
+alias gdisc='git discard'
+
+function gc() {
+    args=$@
+    git commit -m "$args"
+}
+function gca() {
+    args=$@
+    git commit --amend -m "$args"
+}
+
+alias gp='git push'
+
+# git commit -m ... && git push
+function gcp() {
+    args=$@
+    git commit -a -m "$args" && git push -u origin
+}
+alias gcl='git clone'
+alias gch='git checkout'
+alias gl='git log --no-merges'
+# }}}
 
 # Show history
 alias history='fc -l 1'
+# Show ten most used commands
+alias history-stat="history 0 | awk '{print \$2}' | sort | uniq -c | sort -n -r | head"
 
 # List direcory contents
 alias l='ls -la'
@@ -38,7 +74,7 @@ alias tree='tree -Csuh'    #  Nice alternative to 'recursive ls' ...
 if [[ -d ~/dotfiles ]] then
     alias vv='vim ~/dotfiles/vimrc'
     alias bb='vim ~/dotfiles/bashrc; exec bash'
-    alias aa='vim ~/dotfiles/oh-my-zsh/custom/aliases.zsh; exec zsh'
+    alias aa='vim ~/dotfiles/custom/aliases.zsh; exec zsh'
     alias zz='vim ~/dotfiles/zshrc; source ~/.zshrc'
 else
     alias vv='vim ~/.vimrc'
@@ -62,16 +98,20 @@ alias c=clear
 alias x=exit
 
 function stackoverflow() {
-        firefox "http://stackoverflow.com/search?q=$*" &
+    firefox "http://stackoverflow.com/search?q=$*" &
 }
 
 function google() {
-        firefox "http://google.com/search?q=$*" &
+    firefox "http://google.com/search?q=$*" &
 }
 
 if [[ "$(uname)" == "Darwin" ]]; then
-    alias trash='open ~/.trash/'
+    alias rm='trash'
 
     alias gif='echo "mplayer -ao null -loop 0 -ss 0:11:22 -endpos 5 file.avi";
     echo "mplayer -ao null -ss 0:11:22 -endpos 5 file.avi -vo jpeg:outdir=somedir"'
 fi
+
+function size() {
+    du -sh "$@" 2>&1 | grep -v '^du:'
+}
