4098e8475db2123263e96f9c566bace8e20034c8
joshtch <howejosh@gmail.com>
2014-04-14 01:07:54 -0400

    Remove git aliases already in Oh-My-Zsh
    Repetitive redundancy is redundantly repetitive.

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -35,44 +35,7 @@ function rand {
     fi
 }
 
-
-
-# git shortcuts {{{
-alias grep='grep --color=auto'
-
-alias g='git'
-alias ga='git add'
-alias gr='git rm'
-
-alias gf='git fetch'
-alias gu='git pull'
-alias gup='git pull && git push'
-
-alias gs='git status --short'
-alias gd='git diff'
-alias gds='git diff --staged'
-alias gdisc='git discard'
-
-function gc() {
-    args=$@
-    git commit -m "$args"
-}
-function gca() {
-    args=$@
-    git commit --amend -m "$args"
-}
-
-alias gp='git push'
-
-# git commit -m ... && git push
-function gcp() {
-    args=$@
-    git commit -a -m "$args" && git push -u origin
-}
-alias gcl='git clone'
-alias gch='git checkout'
-alias gl='git log --no-merges'
-# }}}
+alias gsl='git diff --stat --color | cat'
 
 # Show history
 alias history='fc -l 1'
