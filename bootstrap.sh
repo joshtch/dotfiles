c2e12b638207e2389ead9091fada2c0f8a9a94f8
joshtch <howejosh@gmail.com>
2014-11-01 22:15:14 -0400

    Update bootstrap to support brew file

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,25 +3,20 @@
 if [[ `uname` == "Darwin" ]]; then
     type -p brew &>/dev/null ||\
     ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"\
-    && brew tap rcmdnk/brewall \
-    && brew install brewall \
-    && brewall set_repo -r https://github.com/joshtch/dotfiles \
-    && brewall update
+    && brew tap rcmdnk/file \
+    && brew install brew-file \
+    && brew file set_repo -r https://github.com/joshtch/dotfiles \
+    && brew file update
 fi
 
-ln -sf ~/dotfiles/vimrc ~/.vimrc
-ln -sf ~/dotfiles/gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/zshrc ~/.zshrc
-ln -sf ~/dotfiles/oh-my-zsh ~/.oh-my-zsh
-ln -sf ~/dotfiles/tmux ~/.tmux
-ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/cask ~/.cask
-ln -sf ~/dotfiles/pentadactylrc ~/.pentadactylrc
-ln -sf ~/dotfiles/pentadactyl ~/.pentadactyl
-ln -sf ~/.vim/bundle/pterosaur/pterosaur.js ~/dotfiles/pentadactyl/plugins/pterosaur.js
+ln -sf vimrc ~/.vimrc
+ln -sf gitconfig ~/.gitconfig
+ln -sf zshrc ~/.zshrc
+ln -sf oh-my-zsh ~/.oh-my-zsh
+ln -sf tmux ~/.tmux
+ln -sf tmux.conf ~/.tmux.conf
+ln -sf cask ~/.cask
+ln -sf pentadactylrc ~/.pentadactylrc
+ln -sf pentadactyl ~/.pentadactyl
 
-mkdir -p ~/.vim/syntax
-mkdir -p ~/.vim/ftdetect
-ln -sf ~/dotfiles/syntax/pentadactyl.vim ~/.vim/syntax
-ln -sf ~/dotfiles/ftdetect/pentadactyl.vim ~/.vim/ftdetect
 chsh -s /bin/zsh && exec zsh
