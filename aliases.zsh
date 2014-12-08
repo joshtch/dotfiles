ec05cb0250a7f88205ed6ed186dd2a2de3ce8112
joshtch <howejosh@gmail.com>
2014-12-08 06:29:02 -0500

    Use directory stack to store history
    auto_pushd - push history onto stack; pushd_ignore_dups - don't allow
    duplicate entries; pushdminus - switch role of '-' and '+' when used
    to specify directory in stack

--- aliases.zsh
+++ aliases.zsh
@@ -5,6 +5,21 @@ alias ...='cd ../..'
 alias ....='cd ../../..'
 alias .....='cd ../../../..'
 
+# Changing/making/removing directory
+setopt auto_pushd
+setopt pushd_ignore_dups
+setopt pushdminus
+alias d='dirs -v | head -10'
+alias 1='cd -'
+alias 2='cd -2'
+alias 3='cd -3'
+alias 4='cd -4'
+alias 5='cd -5'
+alias 6='cd -6'
+alias 7='cd -7'
+alias 8='cd -8'
+alias 9='cd -9'
+
 function urand {
     NUMBYTES="$@"
     valid_num_regexp='[1-4]'
